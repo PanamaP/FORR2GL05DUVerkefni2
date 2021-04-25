4ab6d984f51f95e4d72e5c10020772235499c51e
Elfar Snær <Elfarsnaer@gmail.com>
2021-04-25 16:28:07 +0000

    Add files via upload

---
+++ Scripts/cointurn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cointurn : MonoBehaviour
+{
+    // snyr peninginum i hringi
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 80, 0) * Time.deltaTime);
+    }
+
+    // heldur utan um arekstur a coin
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            
+            gameObject.SetActive(false);
+            FindObjectOfType<GameManager>().CountPoints(1);
+            FindObjectOfType<GameManager>().CoinSound();
+        }
+    }
+
+}
